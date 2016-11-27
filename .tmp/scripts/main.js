3444e778a7cf9f45e022881bfcd0e99e10c4930d
jsahdeva <jagdeep.sahdeva@gmail.com>
2016-11-27 23:05:49 +1100

    corrected parallax

--- .tmp/scripts/main.js
+++ .tmp/scripts/main.js
@@ -1,4 +1,5 @@
 'use strict';
 
 console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
+// $('.parallax-mirror').parallax({imageSrc: './images/Home_Banner_Camps.jpg'});
 //# sourceMappingURL=main.js.map
