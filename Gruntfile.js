d09fa9331e0a9d377b10cb5cd16bad3480b4c0a7
jsahdeva <jagdeep.sahdeva@gmail.com>
2017-05-28 12:11:11 +1000

    Drop 1

--- Gruntfile.js
+++ Gruntfile.js
@@ -206,7 +206,7 @@ module.exports = function (grunt) {
     // Automatically inject Bower components into the HTML file
     wiredep: {
       app: {
-        src: ['<%= config.app %>/index.html'],
+        src: ['<%= config.app %>/*.html'],
         exclude: ['bootstrap.js'],
         ignorePath: /^(\.\.\/)*\.\./
       },
